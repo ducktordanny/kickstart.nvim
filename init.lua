3955e40ae98cc6c552e695eff19c5932d1d1bcd2
Daniel Lazar <lazardani0112@gmail.com>
2023-04-09 23:38:25 +0200

    Add move magic - inspired by ThePrimeagen

--- init.lua
+++ init.lua
@@ -252,6 +252,11 @@ vim.o.scrolloff = 8
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Move around stuff here:
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
