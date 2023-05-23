f1dd247f4b23a0b362ffdb4b42ed6a1a22e46f90
Daniel Lazar <lazardani0112@gmail.com>
2023-05-23 21:59:38 +0200

    Prevent opening empty file with auto-session

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -4,7 +4,8 @@ return {
     require("auto-session").setup {
       log_level = "error",
       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-      pre_save_cmds = {':NvimTreeClose'}
+      pre_save_cmds = {':NvimTreeClose'},
+      post_restore_cmds = {':bd1'}
     }
   end
 }
