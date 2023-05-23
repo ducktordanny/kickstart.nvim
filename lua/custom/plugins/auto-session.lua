77b81392926c7ee0dd65a660272b1f419368f8f5
Daniel Lazar <lazardani0112@gmail.com>
2023-05-23 21:17:57 +0200

    Fix auto session not opening up nvim-tree

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,14 +1,10 @@
-local function restore_nvim_tree()
-  require("nvim-tree.api").tree.open({ focus = false })
-end
-
 return {
   'rmagatti/auto-session',
   config = function()
     require("auto-session").setup {
       log_level = "error",
       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-      post_restore_cmds = {restore_nvim_tree}
+      pre_save_cmds = {':NvimTreeClose'}
     }
   end
 }
